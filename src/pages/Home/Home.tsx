e6e9ebd4deaf13c2491fa3e55af1a17eaf5db7f4
harmitage <anthonyharmitage@pm.me>
2022-03-18 00:50:29 +0400

    Change languages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>F0RTHSP4CE – Хакспейс в Тбилиси</title>
+        <title>F0RTHSP4CE – Hackspace in Tbilisi</title>
         <meta name="description" content="Сообщество энтузиастов и энтузиасток, обьединённых вокруг технологий и искусства. Вместе мы разбираемся в сложных вещах, проводим события, работаем над проектами и просто хорошо проводим время." />
         <meta name="keywords" content="f0rthsp4ce, forthspace, хакспейс, хакспейс тбилиси" />
       </Head>
@@ -14,101 +14,99 @@ const Home: NextPage = () => {
       <h1>F0RTHSP4CE</h1>
       <article>
         <p>
-          F0RTHSP4CE — это <a href="https://ru.wikipedia.org/wiki/Хакерспейс">хакспейс</a>.
-          Сообщество энтузиастов и энтузиасток, обьединенных вокруг технологий и искусства.
-          Вместе мы разбираемся в сложных вещах, проводим события, работаем над проектами и 
-          просто хорошо проводим время.
+          F0RTHSP4CE — is a  <a href="https://en.wikipedia.org/wiki/Hackerspace">hackspace</a>.
+          A community of enthusiasts, focused on technology and art. We are trying to wrap our
+          heads about things that are not easy to understand and to create events.
+          But first and foremost we are having a good time.
         </p>
         <p>
-          Наша миссия – развивать сообщество, в котором все смогут найти что-то для себя,
-          Мы ломаем стены и строим мосты, помогая друг другу и сообществам вокруг нас.
+          Our mission is to develop the community for everybody.
+          We break the walls and build the bridges, and help each other.
         </p>
       </article>
 
       <article>
         <header>
-          <h2>Как с нами взаимодействовать</h2>
+          <h2>How to approach us</h2>
         </header>
         <p>
-          Приходите в гости – пообщаться, познакомиться с пространством или попросить помощи
-          для своего проекта. У всех свои представления о комфортном знакомстве, поэтому к нам
-          можно просто прийти и посмотреть на людей за работой или же сразу присоединиться к
-          заинтересовавшему вас проекту.
+          Come and visit - to talk, to see the space for yourself or maybe even to ask for help.
+          Different people find different modes of communication preferrable, so you may also just visit
+          and work on your project uninterrupted or even just sit and read a book.
         </p>
         <p>
-          Резиденты занимаются спейсом в свободное время на чистом энтузиазме, поэтому у нас нет
-          “дежурных” или “администраторов”. Мы очень любим гостей, но встречаем их только когда кто-то
-          из резидентов готов это сделать. Собираясь заглянуть, напишите заранее в&nbsp;
-          <a href="https://t.me/+w-yypPCNwH1kYTYy">чат</a>,
-          чтобы мы согласовали удобное всем время.
+          Residents are managing space in a free time on pure enthusiasm, we have flat hierarchy
+          and no "administrators" or "managers". We like guests, but we are ready to meet you
+          only when we are ready. If you want to plan a visit, please ping us:&nbsp;
+          <a href="https://t.me/+w-yypPCNwH1kYTYy">chat</a>.
         </p>
         <p>
-          F0RTHSP4CE – не закрытый клуб. Если вы разделяете наши принципы и хотите делать его вместе – станьте резидентом.
+          F0RTHSP4CE – is not a secret club. If you share our principles and want to do something - do it, become a resident.
         </p>
       </article>
 
       <article>
         <header>
-          <h2>Принципы</h2>
+          <h2>Principles</h2>
         </header>
-        <p>Наше сообщество построено на нескольких принципах:</p>
+        <p>Basic principles of our community are:</p>
         <ol>
           <li>
-            <b>Будьте прекрасны по отношению друг к другу</b>
-            <p>Прислушивайтесь ко мнению и потребностям друг друга.</p>
+            <b>Be brilliant to each other</b>
+            <p>Listen to each other's needs and opinions.</p>
           </li>
           <li>
-            <b>Не притесняй и не мешай другим</b>
-            <p>Соблюдай личные границы других участников, старайся договариваться и кооперироваться.</p>
+            <b>Do not oppress and do not bother</b>
+            <p>Be wary of personal boundaries, be ready to make compromises.</p>
           </li>
           <li>
-            <b>Вкладывай больше, чем потребляешь</b>
+            <b>Give more then you take</b>
             <p>
-              F0RTHSP4CE может развиваться только если сообщество делает его лучше.
-              Мы не коворкинг и не обеспечиваем резидентов готовыми условиями для работы.
+              F0RTHSP4CE can grow only if the community makes it better.
+              We are not a co-working per se, so we are all the ones who make the workspaces for each other.
             </p>
           </li>
           <li>
-            <b>Финансовая независимость</b>
-            <p>F0RTHSP4CE не является источником прибыли, а финансовый вклад резидента не влияет на принятие решений.</p>
+            <b>Financial independence</b>
+            <p>F0RTHSP4CE is not a start-up, resident is a resident and you can't by more voting power with donations.</p>
           </li>
           <li>
             <b>Do-ocracy</b>
-            <p>Если хочешь чтобы что-то изменилось – сделай это, но не забывай о других.</p>
+            <p>If you want to change something, do it, but bear in mind the needs of the others.</p>
           </li>
           <li>
-            <b>Запрещено умирать</b>
-            <p>Safety first. Заботьтесь о безопасности себя и окружающих.</p>
+            <b>Dying is strictly forbidden</b>
+            <p>Safety first. Follow the rules and help the others.</p>
           </li>
         </ol>
       </article>
 
       <article>
         <header>
-          <h2>Донейшн</h2>
+          <h2>Donation</h2>
         </header>
         <p>
-          F0RTHSP4CE – это горизонтальное сообщество, существующее только благодаря резидентским взносам и пожертвованиям.
-          Если вы хотите нас поддержать, можно:
+          F0RTHSP4CE is horizontal, so we can exist only with residents' and guests' donations.
+          If you want to help us:
         </p>
         <ul>
-          <li>закинуть наличку в Donation Box</li>
-          <li>задонатить инструменты или материалы</li>
+          <li>drop the cash into the Donation Box</li>
+          <li>donate materials or instruments</li>
         </ul>
       </article>
 
       <article>
         <header>
-          <h2>Контакты</h2>
+          <h2>Contact us</h2>
         </header>
         <ul>
           <li>
-            Тбилиси, ул. Хорава, 18 (Ваке) —&nbsp;
-            <a href="https://yandex.com.ge/maps/-/CCUBYGSX9D">Яндекс</a>&nbsp;
+            Tbilisi, Khorava st, 18 (Vake) —&nbsp;
+            <a href="https://yandex.com.ge/maps/-/CCUBYGSX9D">Yandex</a>&nbsp;
           </li>
           <li>
             <span>Telegram: </span>
-            <a href="https://t.me/+w-yypPCNwH1kYTYy">чат</a>&nbsp;
+            <a href="https://t.me/+w-yypPCNwH1kYTYy">chat</a>&nbsp;
           </li>
           <li>
             <a href="https://github.com/f0rthsp4ce">Github</a>&nbsp;
